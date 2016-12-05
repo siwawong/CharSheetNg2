3a390df803ebb4acd13f4e3b0ea28ec782de5f72
Daren Keck <darenkeck@gmail.com>
2016-12-04 21:31:27 -0800

    Add addstat form, and navigation behavior

--- src/app/addstat/addstat.component.ts
+++ src/app/addstat/addstat.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router }    from '@angular/router';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { CharacterStatsService } from '../character-stats.service';
+import { LoginService }         from '../login.service';
 
 @Component({
   selector: 'app-addstat',
@@ -16,22 +17,47 @@ export class AddstatComponent implements OnInit {
   private value: FormControl;
   private max: FormControl;
   private type: FormControl;
+  private addStatForm: FormGroup;
 
-  constructor(private activatedRoute: ActivatedRoute, 
+  constructor(private activatedRoute: ActivatedRoute,
+              private loginService: LoginService,
               private statService: CharacterStatsService,
-              private formBuilder: FormBuilder, private router: Router) {}
+              private formBuilder: FormBuilder, 
+              private router: Router) {
+  }
 
   ngOnInit() {
-
-
     this.activatedRoute.params.subscribe(params => {
       this.charName = params['name'];
       this.user =     params['user'];
     });
+
+    this.addStatForm = this.formBuilder.group({
+      name: this.name,
+      value: this.value,
+      max: this.max,
+      type: this.type
+    });
   }
 
   submit() {
-    
+    let max = -1;
+
+    if(this.max.value !== undefined) {
+      max = this.max.value;
+    }
+
+    this.statService.addStat(this.loginService.currentUser.id,
+                              this.name.value,
+                              this.value.value,
+                              max,                              
+                              this.type.value);
+
+    this.return();
+  }
+
+  return() {
+    this.router.navigate([this.user, this.charName]);
   }
 
 }
--- src/app/models/character-stat.ts
+++ src/app/models/character-stat.ts
@@ -4,12 +4,4 @@ export class CharacterStat {
     value: number;
     maximum: number;
     type: string;
-
-    constructor(statId: string, statName: string, statValue: number, statMax: number, statType: string) {
-        this.id = statId;
-        this.name = statName;
-        this.value = statValue;
-        this.maximum = statMax;
-        this.type = statType;
-    }
 }
