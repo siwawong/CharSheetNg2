siwawong/CharSheetNg2

commit 8247b7f73ff856623cea19b2bc286cf0cfd40bda

Fixed a Few bugs with CharacterSheet

src/app/store/effects/stat-effects.ts
@@ -12,6 +12,7 @@ import { HttpService } from '../../services/http.service';
 import { StorageService } from '../../services/storage.service';
 
 import * as StatActions from '../actions/stat-actions';
+import * as CharacterActions from '../actions/character-actions';
 import * as NavActions from '../actions/nav-actions';
 import * as fromRoot from '../reducers';
 
@@ -93,15 +94,12 @@ export class StatEffects {
             // Save Dispatched Here?
         });
     
-    @Effect({dispatch: false})
+    @Effect()
     removeAll$: Observable<Action> = this.actions$.ofType(StatActions.REMOVE_ALL)
         .withLatestFrom(this.store$.select(fromRoot.getCharacterId), (action, charId) => charId)
         .map((charId) => {
-            // if (meta.ids.length > 0) {
-            //     this.storage.remStats();                
-            // }
             this.storage.remStats(charId);
-            return null;
+            return new CharacterActions.RemoveAll();
         });
 
     @Effect()
@@ -142,10 +140,12 @@ export class StatEffects {
   
     @Effect()
     update$: Observable<Action> = this.actions$.ofType(StatActions.UPDATE)
-        .map(toPayload)
-        .map((charStat) => {
-            this.storage.setStat(charStat)
-            return new StatActions.UpdateNetwork(charStat);
+        // .map(toPayload)
+        .withLatestFrom(this.store$.select(fromRoot.getStatLateCharId), (action, state) => state)      
+        .map((state) => {
+            this.storage.setStat(state.stat)
+            this.storage.setStatMetaState(state.charId, state.meta.ids, state.meta.selectedId);
+            return new StatActions.UpdateNetwork(state.stat);
         });
 
     @Effect()

src/app/store/reducers/stat-reducer.ts
@@ -63,6 +63,7 @@ export function reducer(state = initialState, action: StatActions.All): StatStat
                 selectedStatId: selectedStatId
             };
         }
+        case StatActions.LOGOUT:
         case StatActions.REMOVE_ALL: {
             return initialState;
         }

src/app/services/storage.service.ts
@@ -157,6 +157,10 @@ export class StorageService {
         this.remStatMetaState(charId);
     }
 
+    clearDB() {
+        this.storage.clear();
+    }
+
     private getItem(KEY: string) {
         return this.storage.get(KEY).then((value) => {
             return value;

src/app/store/actions/character-actions.ts
@@ -18,6 +18,8 @@ export const LOAD_MANY_NETWORK          = '[Character] Load Many Network';
 export const LOAD_MANY_NETWORK_SUCCESS  = '[Character] Load Many Network Success';
 export const LOAD_MANY_NETWORK_ERROR    = '[Character] Load Many Network Error';
 
+export const LOGOUT                     = '[Character] Logout';
+
 export const REMOVE                     = '[Character] Remove';
 export const REMOVE_ERROR               = '[Character] Remove Error';
 export const REMOVE_NETWORK             = '[Character] Remove Network';
@@ -123,6 +125,12 @@ export class LoadManyNetworkError implements Action {
 
     constructor() { }
 };
+
+export class Logout implements Action {
+    readonly type = LOGOUT;
+
+    constructor() { }
+}
 
 export class Remove implements Action {
     readonly type = REMOVE;
@@ -247,6 +255,7 @@ export type All
     | LoadManyNetwork
     | LoadManyNetworkSuccess
     | LoadManyNetworkError
+    | Logout
     | Remove
     | RemoveError
     | RemoveNetwork

src/app/store/actions/stat-actions.ts
@@ -18,6 +18,8 @@ export const LOAD_MANY_NETWORK          = '[Character-Stat] Load Many Network';
 export const LOAD_MANY_NETWORK_SUCCESS  = '[Character-Stat] Load Many Network Success';
 export const LOAD_MANY_NETWORK_ERROR    = '[Character-Stat] Load Many Network Error';
 
+export const LOGOUT                     = '[Character-Stat] Logout';
+
 export const REMOVE                     = '[Character-Stat] Remove';
 export const REMOVE_ERROR               = '[Character-Stat] Remove Error';
 export const REMOVE_NETWORK             = '[Character-Stat] Remove Network';
@@ -122,6 +124,12 @@ export class LoadManyNetworkSuccess implements Action {
 
 export class LoadManyNetworkError implements Action {
     readonly type = LOAD_MANY_NETWORK_ERROR;
+
+    constructor() { }
+}
+
+export class Logout implements Action {
+    readonly type = LOGOUT;
 
     constructor() { }
 }
@@ -249,6 +257,7 @@ export type All
     | LoadManyNetwork
     | LoadManyNetworkSuccess
     | LoadManyNetworkError
+    | Logout
     | Remove
     | RemoveError
     | RemoveNetwork

src/app/store/actions/user-actions.ts
@@ -4,8 +4,8 @@ import { UserState } from '../reducers/user-reducer';
 export const CREATE         = '[User] Create';
 export const CREATE_SUCCESS = '[User] Create Success';
 
-export const DELETE         = '[User] Delete';
-export const DELETE_SUCCESS = '[User] Delete Success';
+export const LOGOUT         = '[User] Logout';
+export const LOGOUT_SUCCESS = '[User] Logout Success';
 
 export const LOGIN          = '[User] Login';
 export const LOGIN_SUCCESS  = '[User] Login Success';
@@ -27,14 +27,14 @@ export class CreateSuccess implements Action {
     constructor(public payload: UserState) { }
 }
 
-export class Delete implements Action {
-    readonly type = DELETE;
+export class Logout implements Action {
+    readonly type = LOGOUT;
 
     constructor() { }
 }
 
-export class DeleteSuccess implements Action {
-    readonly type = DELETE_SUCCESS;
+export class LogoutSuccess implements Action {
+    readonly type = LOGOUT_SUCCESS;
 
     constructor() { }
 }
@@ -78,8 +78,8 @@ export class LoadNone implements Action {
 export type All
     = Create
     | CreateSuccess
-    | Delete
-    | DeleteSuccess
+    | Logout
+    | LogoutSuccess
     | Login
     | LoginSuccess
     | Load

src/app/store/effects/character-effects.ts
@@ -97,18 +97,12 @@ export class CharacterEffects {
             return merge;
         });
     
-    @Effect()
+    @Effect({dispatch: false})
     removeAll$: Observable<Action> = this.actions$.ofType(CharacterActions.REMOVE_ALL)
         .withLatestFrom(this.store$.select(fromRoot.getCharMeta), (action, meta) => meta)
         .map((meta) => {
-            // if (meta.ids.length > 0) {
-            //     this.storage.remChars();              
-            // }
-            // if (meta.selectedId !== null) {
-            //     return new StatActions.RemoveAll();
-            // }
             this.storage.remChars();
-            return new StatActions.RemoveAll();  
+            return null;
             // if for when to remove Network
             // return new StatActions.RemoveAllNetwork();
             // return new StatActions.RemoveAllError(); //Need actual action;

src/app/store/effects/user-effects.ts
@@ -49,17 +49,16 @@ export class UserEffects {
         });
 
     @Effect()
-    deleteNet$: Observable<Action> = this.actions$.ofType(UserActions.DELETE)
+    logout$: Observable<Action> = this.actions$.ofType(UserActions.LOGOUT)
         .withLatestFrom(this.store$.select(fromRoot.getAuth), (action, token) => token)
         .switchMap((authToken) => this.http.logout(authToken))
         .mergeMap((res) => {
-            console.log(`UE, Delete: ${res}`);
             let mergeActions: Action[] = [];
             if (res) {
-                console.log('All Deletes!');
-                this.storage.remUserState();
-                mergeActions.push(new UserActions.DeleteSuccess());
-                mergeActions.push(new CharacterActions.RemoveAll());
+                this.storage.clearDB();
+                mergeActions.push(new UserActions.LogoutSuccess());
+                mergeActions.push(new CharacterActions.Logout());
+                mergeActions.push(new StatActions.Logout());
                 mergeActions.push(new NavActions.Login());
             } else {
                 // Need action for logout error!!!

commit 78036383b0b32ae2fd4d59506687be3f8209238b

Fixed Bug introduced in commit 8247b7f
was incorrectly using selector getStatLatestCharId in StatEffects.UPDATE. This was causing it to pass the last stat in the 'array' as the one that was updated instead of whichever one was supposed to be updated. Added selector getStatMetaCharId as a way to get the necessary meta data to update localStorage and not pass unneccesary info. getStatLastestCharId was renamed to getStatAddedCharId for clarity.

src/app/store/effects/stat-effects.ts
@@ -20,7 +20,7 @@ import * as fromRoot from '../reducers';
 export class StatEffects {
     @Effect()
     add$: Observable<Action> = this.actions$.ofType(StatActions.ADD)
-    .withLatestFrom(this.store$.select(fromRoot.getStatLateCharId), (action, state) => state)
+    .withLatestFrom(this.store$.select(fromRoot.getStatAddedCharId), (action, state) => state)
     .map((state) => this.storage.addStat(
         state.charId, state.meta.ids, state.meta.selectedId, state.stat))    
     .mergeMap((stat)  => {
@@ -49,7 +49,7 @@ export class StatEffects {
     @Effect({dispatch: false})
     saveMany: Observable<Action> = this.actions$.ofType(StatActions.SAVE_MANY)
         .map(toPayload)
-        .withLatestFrom(this.store$.select(fromRoot.getStatLateCharId), (stats, meta) => {
+        .withLatestFrom(this.store$.select(fromRoot.getStatMetaCharId), (stats, meta) => {
             return {
                 stats,
                 state: meta.meta,
@@ -112,7 +112,7 @@ export class StatEffects {
     @Effect()
     remove$: Observable<Action> = this.actions$.ofType(StatActions.REMOVE)
         .map(toPayload)
-        .withLatestFrom(this.store$.select(fromRoot.getStatLateCharId), (payload, meta) => {
+        .withLatestFrom(this.store$.select(fromRoot.getStatMetaCharId), (payload, meta) => {
             return {
                 charId: meta.charId,
                 ids: meta.meta.ids,
@@ -140,8 +140,14 @@ export class StatEffects {
   
     @Effect()
     update$: Observable<Action> = this.actions$.ofType(StatActions.UPDATE)
-        // .map(toPayload)
-        .withLatestFrom(this.store$.select(fromRoot.getStatLateCharId), (action, state) => state)      
+        .map(toPayload)
+        .withLatestFrom(this.store$.select(fromRoot.getStatMetaCharId), (stat, state) => {
+            return {
+                charId: state.charId,
+                stat: stat,
+                meta: state.meta
+            };
+        })      
         .map((state) => {
             this.storage.setStat(state.stat)
             this.storage.setStatMetaState(state.charId, state.meta.ids, state.meta.selectedId);
@@ -163,7 +169,7 @@ export class StatEffects {
 
     @Effect({dispatch: false})
     select$: Observable<Action> = this.actions$.ofType(StatActions.SELECT)
-        .withLatestFrom(this.store$.select(fromRoot.getStatLateCharId), (action, state) => state)
+        .withLatestFrom(this.store$.select(fromRoot.getStatMetaCharId), (action, state) => state)
         .map((state) => {
             this.storage.setStatMetaState(state.charId, state.meta.ids, state.meta.selectedId);
             return null;
@@ -171,7 +177,7 @@ export class StatEffects {
 
     @Effect({dispatch: false})
     unselect$: Observable<Action> = this.actions$.ofType(StatActions.UNSELECT)
-        .withLatestFrom(this.store$.select(fromRoot.getStatLateCharId), (action, state) => state)
+        .withLatestFrom(this.store$.select(fromRoot.getStatMetaCharId), (action, state) => state)
         .map((state) => {
             this.storage.setStatMetaState(state.charId, state.meta.ids, state.meta.selectedId);
             return null;

src/app/store/reducers/index.ts
@@ -45,9 +45,12 @@ export const getStatId       = createSelector(getStatState, fromStats.getSelecte
 export const getStat         = createSelector(getStatState, fromStats.getStat);
 export const getLatestStat   = createSelector(getStatState, fromStats.getLastAdded);
 export const getStatMeta     = createSelector(getStatState, fromStats.getMeta);
-export const getStatLateMeta = createSelector(getStatState, fromStats.getLatestMeta);
+export const getStatAddedMeta = createSelector(getStatState, fromStats.getAddedMeta);
 
-export const getStatLateCharId = createSelector(getCharacterId, getStatLateMeta, (charId, statMeta) => {
+export const getStatMetaCharId = createSelector(getCharacterId, getStatMeta, (charId, meta) => {
+    return { charId, meta };
+});
+export const getStatAddedCharId = createSelector(getCharacterId, getStatAddedMeta, (charId, statMeta) => {
     return { charId, stat: statMeta.stat, meta: statMeta.meta };
 })
 

src/app/store/reducers/stat-reducer.ts
@@ -137,6 +137,6 @@ export const getStats      = createSelector(getIds, getEntities, (ids, entities)
 export const getMeta       = createSelector(getIds, getSelectedId, (ids, selectedId) => {
     return {ids, selectedId};
 });
-export const getLatestMeta = createSelector(getLastAdded, getMeta, (stat, meta) => {
+export const getAddedMeta = createSelector(getLastAdded, getMeta, (stat, meta) => {
     return {stat, meta};
 });