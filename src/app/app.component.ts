391fb627750cc528c475a1016efdee0833259a4c
Margorah <margorah@gmail.com>
2018-01-22 14:48:47 -0500

    Move Slides code from App component to Help Slides page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { StorageService } from './services/storage.service';
 import { AppMenuComponent } from '../components/app-menu/app-menu';
 
 import * as fromRoot from './store/reducers';
-import * as UserActions from './store/actions/user-actions';
+// import * as UserActions from './store/actions/user-actions';
 import * as PreferencesActions from './store/actions/preferences-actions';
 
 @Component({
@@ -19,9 +19,7 @@ import * as PreferencesActions from './store/actions/preferences-actions';
 export class AppComponent {
   title = 'Ng4 CharSheet Store';
   rootPage:any;
-  init: Observable<boolean>;
   private currentTheme: Observable<string>;
-  help: boolean = false;
 
   constructor(platform: Platform,
               statusBar: StatusBar,
@@ -43,22 +41,5 @@ export class AppComponent {
 
   ngOnInit() {
     this.currentTheme = this.store.select(fromRoot.getPrefTheme);
-    this.init = this.store.select(fromRoot.getPrefInit);
-    this.init.subscribe( isInit => {
-      if (isInit) {
-        this.showHelp(true);
-      }
-    });
-  }
-
-  showHelp(help: boolean) {
-    if (help) {
-      this.help = true;
-    }
-  }
-
-  closeHelp() {
-    this.help = false;
-    this.store.dispatch(new PreferencesActions.ChangeInit(false));
   }
 }
--- src/pages/help-slides/help-slides.ts
+++ src/pages/help-slides/help-slides.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+
+import { Store } from '@ngrx/store';
+
+import * as fromRoot from '../../app/store/reducers';
+import * as NavActions from '../../app/store/actions/nav-actions';
+import * as PreferencesActions from '../../app/store/actions/preferences-actions';
 
 /**
  * Generated class for the HelpSlidesPage page.
@@ -15,11 +21,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class HelpSlidesPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private store: Store<fromRoot.State>) {
+  }
+
+  ngOnInit() {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad HelpSlidesPage');
+    // console.log('ionViewDidLoad HelpSlidesPage');
   }
 
+  closeHelp() {
+    this.store.dispatch(new PreferencesActions.ChangeInit(false));
+  }
 }
