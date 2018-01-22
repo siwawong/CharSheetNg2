863711c02faee6a4b9b1643a86d6a9efbb74270d
Margorah <margorah@gmail.com>
2018-01-22 16:34:58 -0500

    Add functionality for Help Slides page to deal with launch from either new install or menu

--- src/pages/help-slides/help-slides.ts
+++ src/pages/help-slides/help-slides.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { IonicPage } from 'ionic-angular';
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
 
 import { Store } from '@ngrx/store';
 
 import * as fromRoot from '../../app/store/reducers';
+import * as NavActions from '../../app/store/actions/nav-actions';
 import * as UserActions from '../../app/store/actions/user-actions';
 import * as PrefActions from '../../app/store/actions/preferences-actions';
 
@@ -13,18 +16,30 @@ import * as PrefActions from '../../app/store/actions/preferences-actions';
   templateUrl: 'help-slides.html',
 })
 export class HelpSlidesPage {
+  fromMenu = false;
+  changeInitSub: Subscription;
+  changeInitObj: Observable<boolean>;
 
   constructor(private store: Store<fromRoot.State>) {
   }
 
   ngOnInit() {
-  }
-
-  ionViewDidLoad() {
+    this.changeInitSub = this.store.subscribe((state) => {
+      this.fromMenu = state.pref.init;
+    });
+    this.changeInitObj = this.store.select(fromRoot.getPrefInit);
   }
 
   closeHelp() {
-    this.store.dispatch(new PrefActions.ChangeInit(false));
-    this.store.dispatch(new UserActions.Load());
+    if (this.fromMenu === true) {
+      this.store.dispatch(new NavActions.Back());
+    } else {
+      this.store.dispatch(new PrefActions.ChangeInit(true));
+      this.store.dispatch(new UserActions.Load()); 
+    }
+  }
+
+  ngOnDestroy() {
+    this.changeInitSub.unsubscribe();
   }
 }
