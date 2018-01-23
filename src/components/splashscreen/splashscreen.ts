f384ff0395c37cd16d2ece74e46f74d03b6b09a9
Margorah <margorah@gmail.com>
2018-01-23 18:31:08 -0500

    Add logic to control displaying the splash screen

--- src/components/splashscreen/splashscreen.ts
+++ src/components/splashscreen/splashscreen.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+
+import { Store } from '@ngrx/store';
+import * as fromRoot from '../../app/store/reducers/index';
+// import { setTimeout } from 'timers';
 
 @Component({
   selector: 'splashscreen',
   templateUrl: 'splashscreen.html'
 })
 export class SplashscreenComponent {
+  splashToggle = false;
+  loadSubscription: Subscription;
 
-  constructor() {
+  constructor(private store: Store<fromRoot.State>) {
   }
 
+  ngOnInit() {
+    this.loadSubscription = this.store.select(fromRoot.getPrefSplash).subscribe(() => {
+        setTimeout(() => {
+          this.splashToggle = true;
+          this.loadSubscription.unsubscribe();
+        }, 2000);
+    });
+  }
 }
