4f8de91cebff127fa0bc47c59ea59c31601db6f8
Margorah <margorah@gmail.com>
2018-02-04 13:02:10 -0500

    Create Stat Button Change component

--- src/pages/character-sheet/character-sheet.module.ts
+++ src/pages/character-sheet/character-sheet.module.ts
@@ -4,15 +4,17 @@ import { CharacterSheetPage } from './character-sheet';
 import { StatComponent } from '../../components/stat/stat';
 import { StatFormChangeComponent } from '../../components/stat-form-change/stat-form-change';
 import { StatSliderChangeComponent } from '../../components/stat-slider-change/stat-slider-change';
+import { StatButtonChangeComponent } from '../../components/stat-button-change/stat-button-change';
 
 @NgModule({
   declarations: [
     CharacterSheetPage,
     StatComponent,
     StatFormChangeComponent,
-    StatSliderChangeComponent
+    StatSliderChangeComponent,
+    StatButtonChangeComponent
   ],
-  entryComponents: [CharacterSheetPage, StatComponent, StatFormChangeComponent, StatSliderChangeComponent],
+  entryComponents: [CharacterSheetPage, StatComponent, StatFormChangeComponent, StatSliderChangeComponent, StatButtonChangeComponent],
   imports: [
     IonicPageModule.forChild(CharacterSheetPage)
   ],
