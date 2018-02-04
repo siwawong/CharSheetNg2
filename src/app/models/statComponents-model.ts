325b2805e374162b90d73fc15e2006fbf7a46a12
Margorah <margorah@gmail.com>
2018-02-04 09:50:20 -0500

    Create stat component lookup model

---
+++ src/app/models/statComponents-model.ts
@@ -0,0 +1,7 @@
+import { StatFormChangeComponent } from '../../components/stat-form-change/stat-form-change';
+import { StatSliderChangeComponent } from '../../components/stat-slider-change/stat-slider-change';
+
+export const STATCOMPONENTS = {
+    'form': StatFormChangeComponent,
+    'slidePlus': StatSliderChangeComponent
+};
